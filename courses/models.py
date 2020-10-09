13284f2dbb0a11d932c68712450e375a7b5c7072
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-08 22:31:53 -0300

    first course model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+
+
+class Course(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    cover = models.ImageField(upload_to='covers/', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('course_detail', args=[str(self.id)])
