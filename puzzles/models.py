49804bf32385953b9f008614790f3fc8d1a22ff7
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-09 13:30:54 -0300

    upgraded model fields for puzzle

--- puzzles/models.py
+++ puzzles/models.py
@@ -8,8 +8,9 @@ from django.urls import reverse
 class Puzzle(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=1000)
-
+    intro = models.TextField('Introduction', blank=True)
+    description = models.TextField('Description', blank=True)
+    is_active = models.BooleanField(default=True)
     cover = models.ImageField(upload_to='puzzles/', blank=True)
 
     created_on = models.DateTimeField(auto_now_add=True)
