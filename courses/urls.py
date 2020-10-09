5163d575bc5e3b98770ebd6bf0e0243dc9ad4fac
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-08 22:32:04 -0300

    wired up list and detail view

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from courses.views import CourseList
+from courses.views import CourseDetail, CourseList
 
 urlpatterns = [
     path('', CourseList.as_view(), name='course_list'),
+    path('<uuid:pk>/', CourseDetail.as_view(), name='course_detail'),
 ]
