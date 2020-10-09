e6f6db978b810d2d4e546933012eaa3cc16ba9f7
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-10-08 22:36:29 -0300

    added list and detail view for course model

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import DetailView, ListView
+
+from courses.models import Course
 
 
-class CourseList(TemplateView):
+class CourseList(ListView):
+    model = Course
+    context_object_name = 'course_list'
     template_name = 'courses/course_list.html'
+
+
+class CourseDetail(DetailView):
+    model = Course
+    context_object_name = 'course'
+    template_name = 'courses/course_detail.html'
